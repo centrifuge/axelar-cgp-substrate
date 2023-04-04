1fb895ed748fd4679ed53cf17c4e6869f42c5288
mustermeiszer <mustermeiszer@posteo.de>
2023-04-04 16:24:05 +0200

    refactor: use licence as header in files

--- gateway/src/traits.rs
+++ gateway/src/traits.rs
@@ -1,3 +1,15 @@
+// Copyright 2023 Centrifuge Foundation (centrifuge.io).
+//
+// This file is part of the axelar-cgp-substrate project.
+// Axelar-cgp-substrate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version (see http://www.gnu.org/licenses).
+// Centrifuge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
 //! Traits used by the Axelar pallet
 
 // ----------------------------------------------------------------------------
